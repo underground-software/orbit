2ba19e480aa46e04c88156f086ddb2036fd8a61b
Michael Burke <michael_burke4@student.uml.edu>
2023-11-27 20:04:00 -0500

    make single elements into tuples + default param

--- db.py
+++ db.py
@@ -7,9 +7,10 @@ import config
 # REG => newusers
 import sys
 
-def _do(cmd, reps, set_=False, get_=False):
+def _do(cmd, reps=(), set_=False, get_=False):
     if config.sql_verbose:
         print("SQL", cmd, file=sys.stderr)
+    reps = (lambda x: x if type(x) is tuple else (x,))(reps)
     dat = None
     con = sqlite3.connect(config.database)
     new = con.cursor()
@@ -26,8 +27,8 @@ def _do(cmd, reps, set_=False, get_=False):
     con.close()
     return dat
 
-_set = lambda cmd, reps: _do(cmd, reps, set_=True, get_=True)
-_get = lambda cmd, reps: _do(cmd, reps, get_=True)
+_set = lambda cmd, reps=(): _do(cmd, reps, set_=True, get_=True)
+_get = lambda cmd, reps=(): _do(cmd, reps, get_=True)
 
 # session table interface
 
@@ -36,7 +37,7 @@ SELECT token, username, expiry
 FROM sessions
 WHERE token = ?;
 """.strip()
-ses_getby_token         = lambda tok: _get(SES_GETBY_TOKEN, (tok,))
+ses_getby_token         = lambda tok: _get(SES_GETBY_TOKEN, tok)
 
 SES_SETEXPIRY_TOKEN="""
 UPDATE sessions
@@ -50,7 +51,7 @@ SELECT token, username, expiry
 FROM sessions
 WHERE username = ?;
 """.strip()
-ses_getby_username      = lambda usn: _get(SES_GETBY_USERNAME, (usn,))
+ses_getby_username      = lambda usn: _get(SES_GETBY_USERNAME, usn)
 
 SES_INS="""
 INSERT INTO sessions (token, username, expiry)
@@ -64,20 +65,20 @@ DELETE FROM sessions
 WHERE token = ?
 RETURNING username;
 """.strip()
-ses_delby_token         = lambda tok: _set(SES_DELBY_TOKEN, (tok,))
+ses_delby_token         = lambda tok: _set(SES_DELBY_TOKEN, tok)
 
 SES_DELBY_USERNAME= """
 DELETE FROM sessions
 WHERE username = ?
 RETURNING username;
 """.strip()
-ses_delby_username      = lambda usn: _set(SES_DELBY_USERNAME, (usn,))
+ses_delby_username      = lambda usn: _set(SES_DELBY_USERNAME, usn)
 
 SES_GET="""
 SELECT token, username, expiry
 FROM sessions;
 """.strip()
-ses_get                 = lambda    : _get(SES_GET, ())
+ses_get                 = lambda    : _get(SES_GET)
 
 # users table interface
 
@@ -86,7 +87,7 @@ SELECT pwdhash
 FROM users
 WHERE username = ?;
 """.strip()
-usr_pwdhashfor_username = lambda usn: _get(USR_PWDHASHFOR_USERNAME, (usn,))
+usr_pwdhashfor_username = lambda usn: _get(USR_PWDHASHFOR_USERNAME, usn)
 
 USR_INS="""
 INSERT INTO users (username, pwdhash, lfx, student_id)
@@ -99,7 +100,7 @@ DELETE FROM users
 WHERE username = ?
 RETURNING username;
 """.strip()
-usr_delby_username      = lambda usn: _set(USR_DELBY_USERNAME, (usn,))
+usr_delby_username      = lambda usn: _set(USR_DELBY_USERNAME, usn)
 
 USR_SETPWDHASH_USERNAME="""
 UPDATE users
@@ -112,14 +113,14 @@ USR_GET="""
 SELECT id, username, pwdhash, lfx, student_id
 FROM users;
 """.strip()
-usr_get                 = lambda    : _get(USR_GET, ())
+usr_get                 = lambda    : _get(USR_GET)
 
 USR_GETBY_USERNAME="""
 SELECT id, username, pwdhash, lfx, student_id
 FROM users
 WHERE username = ?;
 """.strip()
-usr_getby_username      = lambda usn: _get(USR_GETBY_USERNAME, (usn,))
+usr_getby_username      = lambda usn: _get(USR_GETBY_USERNAME, usn)
 
 
 USR_SET_LFX="""
@@ -127,21 +128,21 @@ UPDATE users
 SET lfx = True
 WHERE username = ?;
 """.strip()
-usr_set_lfx             = lambda usn: _set(USR_SET_LFX, (usn,))
+usr_set_lfx             = lambda usn: _set(USR_SET_LFX, usn)
 
 USR_SET_NOLFX="""
 UPDATE users
 SET lfx = False
 WHERE username = ?;
 """.strip()
-usr_set_nolfx           = lambda usn: _set(USR_SET_NOLFX, (usn,))
+usr_set_nolfx           = lambda usn: _set(USR_SET_NOLFX, usn)
 
 USR_GETIF_LFX_USERNAME="""
 SELECT lfx
 FROM users
 WHERE username = ?;
 """.strip()
-usr_getif_lfx_username  = lambda usn: _get(USR_GETIF_LFX_USERNAME, (usn,))
+usr_getif_lfx_username  = lambda usn: _get(USR_GETIF_LFX_USERNAME, usn)
 
 # submission table interface
 
@@ -158,7 +159,7 @@ SUB_GET="""
 SELECT *
 FROM submissions;
 """.strip()
-sub_get                 = lambda    : _get(SUB_GET, ())
+sub_get                 = lambda    : _get(SUB_GET)
 
 SUB_INS="""
 INSERT INTO submissions (sub_id, username, timestamp, _from, _to, email_ids, subjects)
@@ -171,14 +172,14 @@ SELECT sub_id, username, timestamp, _from, _to, email_ids, subjects
 FROM submissions
 WHERE sub_id = ?;
 """.strip()
-sub_getby_subid         = lambda sid: _get(SUB_GETBY_SUBID, (sid,))
+sub_getby_subid         = lambda sid: _get(SUB_GETBY_SUBID, sid)
 
 SUB_GETBY_USERNAME="""
 SELECT sub_id, username, timestamp, _from, _to, email_ids, subjects
 FROM submissions
 WHERE user = ?;
 """.strip()
-sub_getby_username      = lambda usr: _get(SUB_GETBY_USERNAME, (usr,))
+sub_getby_username      = lambda usr: _get(SUB_GETBY_USERNAME, usr)
 
 # assignment table interface
 
@@ -187,20 +188,20 @@ SELECT web_id, email_id
 FROM assignments
 WHERE web_id = ?;
 """.strip()
-asn_getby_webid         = lambda wid: _get(ASN_GETBY_WEBID, (web_id,))
+asn_getby_webid         = lambda wid: _get(ASN_GETBY_WEBID, web_id)
 
 ASN_GETBY_EMAILID="""
 SELECT web_id, email_id
 FROM assignments
 WHERE email_id = ?;
 """.strip()
-asn_getby_email_id      = lambda eid: _get(ASN_GETBY_EMAIL_ID, (eid,))
+asn_getby_email_id      = lambda eid: _get(ASN_GETBY_EMAIL_ID, eid)
 
 ASN_GET="""
 SELECT *
 FROM assignments;
 """.strip()
-asn_get                 = lambda    : _get(ASN_GET, ())
+asn_get                 = lambda    : _get(ASN_GET)
 
 # registration table inferface
 
@@ -215,10 +216,10 @@ SELECT registration_id, username, password
 FROM newusers
 WHERE student_id = ?;
 """.strip()
-reg_getby_stuid         = lambda sid: _set(REG_GETBY_STUDENTID, (sid,))
+reg_getby_stuid         = lambda sid: _set(REG_GETBY_STUDENTID, sid)
 
 REG_DELBY_REGID="""
 DELETE FROM accounts
 WHERE id = ?;
 """.strip()
-reg_delby_regid         = lambda rid: _get(REG_DEL_BY_REGISRATION_ID, (rid,))
+reg_delby_regid         = lambda rid: _get(REG_DEL_BY_REGISRATION_ID, rid)
