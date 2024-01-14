7ddfbd3a57a302aeacd3916b7208bab6cd43b05c
Joel Savitz <jsavitz@redhat.com>
2024-01-14 00:49:24 -0500

    visit PCP and get physical :)
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -5,14 +5,14 @@ cd "$(dirname "$0")"
 curl --insecure "https://127.0.0.1"
 
 # Test pop3
-POP3_PORT="$(./config.py pop3_port)"
+POP3_PORT="$(../config.py pop3_port)"
 printf "QUIT\r\n"           | \
   nc localhost "$POP3_PORT" | \
 	diff -up - <(printf "220 SMTP server ready\r\n221 Goodbye\r\n")
 
 
 # Test smtp
-SMTP_PORT="$(./config.py smtp_port)"
+SMTP_PORT="$(../config.py smtp_port)"
 printf "QUIT\r\n"           | \
   nc localhost "$SMTP_PORT" | \
 	diff -up - <(printf "220 SMTP server ready\r\n221 Goodbye\r\n")
