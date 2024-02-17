318a14de677c87d61d1e9b6869aef3ec6b1f69ff
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2024-02-17 12:04:32 -0500

    warpdrive.sh: genericize container name with env var
    Depending on how the container is spawned, it might have a
    different name. Allow the user to specify one of their choosing
    by setting an env var while maintaining backwards compat with
    a default value.

--- warpdrive.sh
+++ warpdrive.sh
@@ -5,8 +5,9 @@
 cd "$(dirname "$0")"
 
 DOCKER=${DOCKER:-sudo docker}
+CONTAINER=${CONTAINER:-docker_orbit_1}
 
-cat <<EOF | $DOCKER exec -i docker_orbit_1 /bin/sh
+cat <<EOF | $DOCKER exec -i $CONTAINER /bin/sh
 . /radius-venv/bin/activate
 /orbit/hyperspace.py $@
 EOF
